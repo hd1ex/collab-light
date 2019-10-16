4458ef2929655dd29ee31f2a26c1b10e8a055fa7
Alexander <alexander.sommer@hadiko.de>
2019-10-16 21:19:28 +0200

    Change host address from localhost to parameter

--- client.bash
+++ client.bash
@@ -1,8 +1,11 @@
 #!/bin/bash
+# First param: host
+# Second param: command
+
 function get_hash() {
     echo "$1" | md5sum | cut -f1 -d " "
 }
 
 name="$(hostname)@$(ifconfig | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*' | grep -v '127.0.0.1' | tail -n 1)"
 echo $name
-echo "$name:$(get_hash $name):$1" | ncat localhost 7777
+echo "$name:$(get_hash $name):$2" | ncat $1 7777
